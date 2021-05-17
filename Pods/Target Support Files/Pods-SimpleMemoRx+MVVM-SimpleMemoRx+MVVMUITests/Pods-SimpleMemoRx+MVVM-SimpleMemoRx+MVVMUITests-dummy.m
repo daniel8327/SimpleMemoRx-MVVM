846c1aca4926d6e4d4ecded4c91ce0f58872fb1c
장태현 <danielchang27@gmail.com>
2021-05-18 08:14:36 +0900

    Pod 추가시킴

---
+++ Pods/Target Support Files/Pods-SimpleMemoRx+MVVM-SimpleMemoRx+MVVMUITests/Pods-SimpleMemoRx+MVVM-SimpleMemoRx+MVVMUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SimpleMemoRx_MVVM_SimpleMemoRx_MVVMUITests : NSObject
+@end
+@implementation PodsDummy_Pods_SimpleMemoRx_MVVM_SimpleMemoRx_MVVMUITests
+@end
